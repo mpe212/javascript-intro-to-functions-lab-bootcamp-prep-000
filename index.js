30ffbf83aea17df97473787dc01ec2d6db550e84
Marcia Elyseu <marcia.pelyseu@gmail.com>
2018-05-20 21:59:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === true){
+  if (string.isLowerCase() === true){
     return "I can\'t hear you!";
   };
 }
